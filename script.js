5349d6a84582b2b8052b42dfcb374addb8af530e
anvin <anvinkaippillil123@gmail.com>
2023-10-01 21:20:14 +0530

    add button to close side-nav

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+//section: open and close side-navigation
+
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
+    //open navigation
     document.getElementById("side-button").addEventListener("click", function() {
         document.getElementById("side-wrapper").classList.toggle("active");
     });
+    //close navigation
     document.getElementById("page-content-wrapper").addEventListener("click", function() {
+        document.getElementById("side-wrapper").classList.remove("active");
+    });
+    document.getElementById("nav-close").addEventListener("click", function(){
         document.getElementById("side-wrapper").classList.remove("active");
     });
   });
 
-// goto a certain section of the page
+// section: goto a certain section of the page
 document.addEventListener('DOMContentLoaded', function () {
     const scrollButton = document.getElementById('arrow-icon');
     scrollButton.addEventListener('click', function () {
