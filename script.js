0e085705c4b2b3e54d113d45b625b08617dbc470
anvin <anvinkaippillil123@gmail.com>
2023-10-03 07:51:26 +0530

    add buttons to toggle program links

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 //section: open and close side-navigation
 
 // Wait for the DOM to be fully loaded
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
     //open navigation
-    document.getElementById("side-button").addEventListener("click", function() {
+    document.getElementById("side-button").addEventListener("click", function () {
         document.getElementById("side-wrapper").classList.toggle("active");
     });
-    //close navigation
-    document.getElementById("page-content-wrapper").addEventListener("click", function() {
+
+    //close navigation on clicking page
+    document.getElementById("page-content-wrapper").addEventListener("click", function () {
         document.getElementById("side-wrapper").classList.remove("active");
     });
-    document.getElementById("nav-close").addEventListener("click", function(){
+
+    //close navigation on close button click
+    document.getElementById("nav-close").addEventListener("click", function () {
         document.getElementById("side-wrapper").classList.remove("active");
     });
-  });
 
-//   section: open and close resource links in navigation
+    //   section: open and close resource and program links in side-navigation
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("resource-button").addEventListener("click", function() {
+    // open resource segment
+    document.getElementById("resource-button").addEventListener("click", function () {
         document.getElementById("select-program").classList.toggle("active");
+
+        // close BSc and BCA section when clicking resource button 
+        document.getElementById("select-category1").classList.remove("active");
+        document.getElementById("select-category2").classList.remove("active");
     });
 
-  });
+    // open BSc segment 
+    document.getElementById("program-button1").addEventListener("click", function () {
+        document.getElementById("select-category1").classList.toggle("active");
+    });
 
-// section: goto a certain section of the page
-document.addEventListener('DOMContentLoaded', function () {
+    // open BCA segment 
+    document.getElementById("program-button2").addEventListener("click", function () {
+        document.getElementById("select-category2").classList.toggle("active");
+    });
+
+    // section: goto a certain section of the page
     const scrollButton = document.getElementById('arrow-icon');
     scrollButton.addEventListener('click', function () {
         const targetSection = document.getElementById('section-2');
         targetSection.scrollIntoView({ behavior: 'smooth' });
     });
+
 });
+
+
+
 
