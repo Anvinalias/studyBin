feac0ae276c493f30e408462362842392fa00f60
anvin <anvin@xubuntu.myguest.virtualbox.org>
2023-12-12 13:05:31 +0530

    Create admin authentication

--- admin/signin/signin.php
+++ admin/signin/signin.php
@@ -1,3 +1,29 @@
+<?php session_start(); 
+include_once('../../includes/config.php');
+// Code for login 
+if(isset($_POST['login']))
+{
+  $adminusername=$_POST['name'];
+  $pass=md5($_POST['password']);
+$ret=mysqli_query($con,"SELECT * FROM users WHERE username='$adminusername' and password='$pass' and role = 'admin'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+$extra="../admin.php";
+$_SESSION['login']=$_POST['username'];
+$_SESSION['adminid']=$num['id'];
+echo "<script>window.location.href='".$extra."'</script>";
+exit();
+}
+else
+{
+echo "<script>alert('Invalid username or password');</script>";
+$extra="signin.php";
+echo "<script>window.location.href='".$extra."'</script>";
+exit();
+}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,24 +46,24 @@
       </section>
       <section class="form-container">
          <h1>Admin Login</h1>
-         <form spellcheck="false" autocomplete="off">
-            <div class="form-inputs">
-               <div class="input-group">
-                  <div class="error hide">Name cannot be empty</div>
-                  <label for="name"><i class="fa-regular fa-user"></i></label>
-                  <input id="name" type="text" name="name" placeholder="Username" required />
-               </div>
-            </div>
-            <div class="input-group">
-               <div class="error hide">Password is not strong enough</div>
-               <label for="password"><img class="lock" src="images/lock.svg" alt="lock" /></label>
-               <input id="password" type="password" placeholder="Password" name="password" required />
-            </div>
-            </div>
-            <button>
-               <span>Continue</span>
-            </button>
-         </form>
+         <form method="post">
+   <div class="form-inputs">
+      <div class="input-group">
+         <div class="error hide">Name cannot be empty</div>
+         <label for="name"><i class="fa-regular fa-user"></i></label>
+         <input id="name" type="text" name="name" placeholder="Username" required />
+      </div>
+   </div>
+   <div class="input-group">
+      <div class="error hide">Password is not strong enough</div>
+      <label for="password"><img class="lock" src="images/lock.svg" alt="lock" /></label>
+      <input id="password" type="password" placeholder="Password" name="password" required />
+   </div>
+   <button name="login" type="submit">
+      <span>Continue</span>
+   </button>
+</form>
+
       </section>
    </main>
 </body>
